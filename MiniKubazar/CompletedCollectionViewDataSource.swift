ef18224e32f115cf7cac1f7b7f29790837abe292
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-10-04 01:24:50 +0200

    Added NSCache and NSOperationQueue code in Completed Collection View Data Source.

--- MiniKubazar/CompletedCollectionViewDataSource.swift
+++ MiniKubazar/CompletedCollectionViewDataSource.swift
@@ -15,6 +15,10 @@ class CompletedCollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
     var completedHaikus = [ActiveHaiku]()
     
+    var imageCache = NSCache()
+    
+    var imageDownloadingQueue = NSOperationQueue()
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         
@@ -30,19 +34,33 @@ class CompletedCollectionViewDataSource: NSObject, UICollectionViewDataSource {
         
         let imageURL = completedHaiku.imageURLString
         
-        let haikuImageRef = FIRStorage.storage().referenceForURL(imageURL)
-        haikuImageRef.dataWithMaxSize(1 * 3000 * 3000) { (data, error) in
-            if (error != nil) {
-                print(error)
-                print("something wrong with completed haiku image from storage. check completed collection view data source code")
-            } else {
-                let haikuImage = UIImage(data: data!)
-                //                cell.imageView.image = haikuImage
-                cell.updateWithImage(haikuImage)
-                cell.firstHaikuLine.text = completedHaiku.firstLineString
-                
-                
-            }
+        let cachedImage = imageCache.objectForKey(imageURL) as? UIImage
+        
+        if ((cachedImage) != nil) {
+            cell.updateWithImage(cachedImage)
+        } else {
+        
+        self.imageDownloadingQueue.addOperationWithBlock({
+            let haikuImageRef = FIRStorage.storage().referenceForURL(imageURL)
+            haikuImageRef.dataWithMaxSize(1 * 3000 * 3000) { (data, error) in
+                if (error != nil) {
+                    print(error)
+                    print("something wrong with completed haiku image from storage. check completed collection view data source code")
+                } else {
+                    let haikuImage = UIImage(data: data!)
+                    //                cell.imageView.image = haikuImage
+                    cell.updateWithImage(haikuImage)
+                    cell.firstHaikuLine.text = completedHaiku.firstLineString
+                    
+                    if haikuImage != nil {
+                        self.imageCache.setObject(haikuImage!, forKey: imageURL)
+                    }
+                    
+                    
+                }
+            }})
+            
+        
         }
 
         
